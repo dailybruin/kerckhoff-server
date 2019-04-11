3e44ae5cd2ea7e04db4cbbe333286484acc0e1c2
Rohan Khajuria <rkhajuria99@gmail.com>
2019-04-10 21:14:45 -0700

    line endings

--- kerckhoff/comments/views.py
+++ kerckhoff/comments/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from .models import Comment
 from .serializers import CommentSerializer
 
+
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
