4d66e55d55aa38fdc61e0a7a26bfac58874e40df
屈定春 <helloworld@owners-MacBook-Pro-5.local>
2019-11-13 19:34:35 -0800

    Check Auth

--- kerckhoff/comments/views.py
+++ kerckhoff/comments/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
 from .models import Comment
 from kerckhoff.packages.models import Package
 from .serializers import CommentSerializer
@@ -7,7 +9,8 @@ from .serializers import CommentSerializer
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-
+    
+    @permission_classes([IsAuthenticated])
     def perform_create(self, serializer):
         package = Package.objects.get(slug=self.kwargs["package_slug"])
         serializer.save(created_by=self.request.user, package=package)
